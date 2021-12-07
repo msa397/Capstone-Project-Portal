f0d7bf763e8b2ee3732d0182bbca4a2f084b5253
Michael Anderson <91154019+msa397@users.noreply.github.com>
2021-12-07 09:48:24 -0500

    Add files via upload

--- database/init.sql
+++ database/init.sql
@@ -5,6 +5,21 @@ TODO:
     See Comments
 
 Modified: 10/30/21 by Michael
+
+
+
+
+INSERT INTO User_Role(role_id, role_name) VALUES
+    (1, 'Student'),
+    (2, 'Administrator');
+
+INSERT INTO Permission (perm_id, perm_mod, perm_desc) VALUES
+    (1, 'STU', 'Project Preferences'),
+    (2, 'ADM', 'Post Project');
+
+INSERT INTO Role_Permissions (role_id, perm_id) VALUES
+    (1, 1),
+    (2, 2);
 */
 
 
@@ -12,14 +27,33 @@ Modified: 10/30/21 by Michael
 CREATE DATABASE IF NOT EXISTS Project_Portal;
 
 use Project_Portal;
--- Holds student information
--- Student ID is automatically incremented by 1 for each row
-CREATE TABLE IF NOT EXISTS Student (
-    student_id INTEGER AUTO_INCREMENT PRIMARY KEY, -- ask professor what identifier he wants for students
+-- Holds User information
+-- User ID is automatically incremented by 1 for each row
+CREATE TABLE IF NOT EXISTS User (
+    user_id INTEGER AUTO_INCREMENT PRIMARY KEY, -- ask professor what identifier he wants for User
+    email VARCHAR(30) NOT NULL,
+    pass VARCHAR(30) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL
-    -- do students need to log in? if so, add username + password
+    last_name VARCHAR(20) NOT NULL,
+    role_id int(11) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS User_Role(
+    role_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Permission(
+    perm_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    perm_mod VARCHAR(10) NOT NULL,
+    perm_desc VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Role_Permissions(
+    role_id int(11) NOT NULL PRIMARY KEY,
+    perm_id int(11) NOT NULL
+);
+
 
 -- Holds project information
 -- Project ID is automatically incremented by 1 for each row
@@ -37,17 +71,17 @@ CREATE TABLE IF NOT EXISTS Project (
 
 -- Holds student project preferences
 CREATE TABLE IF NOT EXISTS Preferences (
-    student_id INTEGER,
+    user_id INTEGER,
     project_id INTEGER,
     preference INTEGER NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES Student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (project_id) REFERENCES Project (project_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Holds group assignments
 CREATE TABLE IF NOT EXISTS Group_Assignment (
-    student_id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     project_id INTEGER,
-    FOREIGN KEY (student_id) REFERENCES Student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (project_id) REFERENCES Project (project_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
